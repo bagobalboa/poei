4fd8cc4621537e490303b6648d6c3bdd5ee00155
Olivier <bago75@hotmail.fr>
2022-09-09 11:27:15 +0200

    page object model and option hover function

--- src/test/java/TpPageObject.java
+++ src/test/java/TpPageObject.java
@@ -2,6 +2,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
@@ -37,6 +38,27 @@ public class TpPageObject {
                 .openCart()
                 .selectQuantity(2);
 
+        // Assert.assertEquals(cartPage.getSubTotal(), "1000");
+
+
+    }
+
+    @Test
+    public void Test2() {
+        HomePage homePage = new HomePage(driver);
+        homePage.acceptCookie();
+
+        WebElement loginButton = driver.findElement(By.cssSelector("#nav-link-accountList"));
+
+        Actions actions = new Actions(driver);
+        actions.moveToElement(loginButton);
+        actions.perform();
+
+        try {
+            Thread.sleep(4000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
 
     }
 
